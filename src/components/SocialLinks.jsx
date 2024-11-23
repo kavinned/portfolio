375f021e6fc36a501b91ec7a780986bdd7583665
Kavin <kavin.ned@gmail.com>
2024-11-23 19:59:21 +0800

    feat: update hrefs

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -3,19 +3,19 @@ import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
 
 const links = [
 	{
-		href: "https://github.com",
+		href: "https://github.com/kavinned",
 		icon: <FiGithub />,
 		label: "Github",
 		delay: 0,
 	},
 	{
-		href: "https://linkedin.com",
+		href: "https://www.linkedin.com/in/kavin-nedumaran/",
 		icon: <FiLinkedin />,
 		label: "LinkedIn",
 		delay: 0.15,
 	},
 	{
-		href: "mailto:your.email@example.com",
+		href: "mailto:kavin.ned@gmail.com",
 		icon: <FiMail />,
 		label: "Email",
 		delay: 0.3,
